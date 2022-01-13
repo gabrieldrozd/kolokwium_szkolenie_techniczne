dca41137ab62c2893ac9aad6f8d3d2ce85849048
Gabriel <drozd.gj@gmail.com>
2022-01-13 19:42:44 +0100

    project files added

---
+++ WebApplication1/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public int TypeId { get; set; }
+        public virtual BookType TypeId { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Description { get; set; }
+        public int ReleaseDate { get; set; }
+    }
+}
